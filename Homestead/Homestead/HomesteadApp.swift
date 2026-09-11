5efbea71e19632480cc756c34726dc6249896b73
isilantev <isilantev@quantumbrains.com>
2026-09-11 16:36:53 +0200

    Add Xcode app target

---
+++ Homestead/Homestead/HomesteadApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomesteadApp.swift
+//  Homestead
+//
+//  Created by Sheizenger on 9/11/26.
+//
+
+import SwiftUI
+
+@main
+struct HomesteadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
